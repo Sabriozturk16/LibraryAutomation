4a00caa029c9ad10eac88515b6da5983e0507796
SABRİ ÖZTÜRK <sabriozturk2@posta.mu.edu.tr>
2023-05-10 16:58:44 +0300

    Migration Yapıldı.

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -6,11 +6,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DataAccessLayer.Concrete
 {
+    
     public class Context : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=LibraryAutomation;trusted_connection=true;");
+        }
         public DbSet<About> Abouts {get; set;}
         public DbSet<Category> Categories { get; set; }
         public DbSet<Contact> Contacts { get; set; }
